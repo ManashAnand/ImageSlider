32c3f05c358b3fab09d8fc536a463fb106556663
MANASH <anandmanash321@gmail.com>
2022-06-04 00:33:43 +0530

    initial commit

---
+++ designLogic.js
@@ -0,0 +1,14 @@
+panel = document.querySelectorAll('.panel');
+// console.log(panel);
+panel.forEach(element => {
+    element.addEventListener("mouseover",()=>{
+            element.classList.add("active");
+            element.classList.add("open");
+    });
+});
+panel.forEach(element => {
+    element.addEventListener("mouseout",()=>{
+            element.classList.remove("active");
+            element.classList.remove("open");
+    });
+});
